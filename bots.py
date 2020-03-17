e881adbc7f4bb9046a10a11512106852eba13d41
Mihail <gusev.mihail.34@gmail.com>
2020-03-18 00:00:43 +0300

    improve DroneBuilder

--- bots.py
+++ bots.py
@@ -16,18 +16,22 @@ class DroneBuilder(sc2.BotAI):
 
     async def on_step(self, iteration: int):
         for loop_larva in self.larva:
-            if self.supply_left <= self.supply_left_to_overlord:  # few supply, build overlord for the future
 
+            # at first, check, need we build overlords
+            if self.supply_left <= self.supply_left_to_overlord:  # few supply, build overlord for the future
                 if self.can_afford(UnitTypeId.OVERLORD):
                     self.do(loop_larva.train(UnitTypeId.OVERLORD), subtract_cost=True, subtract_supply=True)
                 else:  # can't train overlord on this step
-                    break
+                    break  # no build anything before overlord
 
-            else:  # have many supply, can build drones
+            # have many supply, can build drones
+            if self.can_afford(UnitTypeId.DRONE):
+                self.do(loop_larva.train(UnitTypeId.DRONE), subtract_cost=True, subtract_supply=True)
+            else:  # can't train drons on this step
+                break
 
-                if self.can_afford(UnitTypeId.DRONE):
-                    self.do(loop_larva.train(UnitTypeId.DRONE), subtract_cost=True, subtract_supply=True)
-                else:  # can't train drons on this step
-                    break
+        # TODO use this information in code, that check we need build overlords (line 21)
+        print(self.already_pending(UnitTypeId.OVERLORD))
 
-
+# class ZerglingRush(sc2.BotAI):
+#     async def on_step(self, iteration: int):
